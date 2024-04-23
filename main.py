1e42bb0880954945fb732bec318f99eb7c8349f4
Oleg <89175955650@mail.ru>
2024-04-23 19:37:12 +0300

    First commit to Git.

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+from pprint import pprint
+import json
+import time
+
+
+def make_header():
+    pass
+
+
+def make_response():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
